5c11c92b1063e7ce8eeed5bb7e9f8f138d92e2b5
Ubuntu <ubuntu@ip-172-31-20-174.us-west-2.compute.internal>
2025-02-21 20:08:04 +0000

    updating commits

--- Classifiers/image_classifier.py
+++ Classifiers/image_classifier.py
@@ -47,28 +47,14 @@ def process_table(table_name):
     try:
         print(f"Processing table: {table_name}")
         rows = session.execute(f"SELECT * FROM {table_name}")
-        # cpu_utilizations = []
-        # memory_utilizations = []
 
         for row in rows:
-            # Record CPU and memory usage
-            # cpu_util_per_core = psutil.cpu_percent(interval=None, percpu=True)
-            # mem_util = psutil.virtual_memory().percent
-            # cpu_utilizations.append(cpu_util_per_core)
-            # memory_utilizations.append(mem_util)
-
             classification_label = classify_image(row.image_data)
             category_name = MobileNet_V2_Weights.DEFAULT.meta["categories"][classification_label]
             print(f"Image ID {row.id} classified as: {category_name}")
 
-        # Calculate average CPU and memory utilization
-        # avg_cpu_util = [sum(core) / len(core) for core in zip(*cpu_utilizations)]
-        # avg_memory_util = sum(memory_utilizations) / len(memory_utilizations) if memory_utilizations else 0
-
         return {
             "status": "success"
-            # "avg_cpu_util": avg_cpu_util,
-            # "avg_memory_util": avg_memory_util,
         }
 
     except Exception as e:
@@ -77,19 +63,6 @@ def process_table(table_name):
 
 # Flask server to handle incoming requests
 app = Flask(__name__)
-
-'''
-@app.route("/classify", methods=["POST"])
-def classify():
-    print("enter classify")
-    data = request.json
-    if not data or "table_name" not in data:
-        return jsonify({"status": "error", "message": "Table name is required"}), 400
-
-    table_name = data["table_name"]
-    result = process_table(table_name)
-    return jsonify(result)
-'''
 
 import time
 
@@ -110,14 +83,6 @@ def classify():
     timestamp_end = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(end_time))
     print(f"Classification ended at {timestamp_end}")
     return jsonify(result)
-
-'''
-@app.route('/health')
-def health_check():
-    # Get CPU utilization
-    cpu_utilization = psutil.cpu_percent(interval=None)
-    return jsonify({"cpu_utilization": cpu_utilization})
-'''
 
 if __name__ == "__main__":
     # Start the Flask server and listen for incoming HAProxy requests
